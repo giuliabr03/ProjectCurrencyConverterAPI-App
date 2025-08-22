14bf7ec297f59228dbafd3d9a86908ab50a3af3b
giuliabr03 <bragagiulia03@gmail.com>
2025-08-22 11:55:19 -0300

    identifica o endpoint da API

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
@@ -1,6 +1,13 @@
 // Classe responsável por configurar e fornecer uma instância retrofit
 package com.example.projectcurrencyconverterapi;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface ExchangeRateService {
+    /**
+     * Método para obter taxas de câmbio recentes
+     */
 }
 
