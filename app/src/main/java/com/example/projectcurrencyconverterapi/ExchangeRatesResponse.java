bd4325e194c33816305cc6a35dfc1a2068530731
giuliabr03 <bragagiulia03@gmail.com>
2025-08-22 10:08:52 -0300

    cria arquivo de configuração de respota à API

---
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
@@ -0,0 +1,6 @@
+// Modelo de dados (DtD - Data Transfer Object) que representa a resposta json da API
+// Json
+package com.example.projectcurrencyconverterapi;
+
+public class ExchangeRatesResponse {
+}
