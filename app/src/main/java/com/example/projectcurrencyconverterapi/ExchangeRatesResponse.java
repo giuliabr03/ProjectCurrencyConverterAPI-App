1104bc504f34173dd1801ab1e7d5776ec1f13b35
giuliabr03 <bragagiulia03@gmail.com>
2025-09-03 09:09:02 -0300

    Importa classe map

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
@@ -2,5 +2,18 @@
 // Json
 package com.example.projectcurrencyconverterapi;
 
+import java.util.HashMap; //import a classe Map da biblioteca Java, usada para armazenar pares da chave-valor
+import java.util.Map; // Importa a classe Map da biblioteca Java, usada para armazenar pares chave-valor
+
 public class ExchangeRatesResponse {
+    // declara um mapa que arazena taxas de conversão em moedas. (ex. USd, EUR, GBP)
+    // a chave (String) representa o codigp da Moeda e o valor (Double) representa a taxa de conversão
+
+    private Map<String, Double> conversion_rates = new HashMap<>();
+    //Metodo publico para acessar as taxas de cpnversão
+    // Retona o mapa de taxas de conversão[
+
+    public Map<String, Double> getConversion_rates (){
+        return conversion_rates;
+    }
 }
