96d017af134598ee610a4a80ffa7ffc36fb3c49a
giuliabr03 <bragagiulia03@gmail.com>
2025-09-03 08:54:56 -0300

    Adiciona as substituições

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
@@ -1,4 +1,5 @@
 // Classe responsável por configurar e fornecer uma instância retrofit
+//interface qe identifica o endpoint da API que será consumida pelo retrofit
 package com.example.projectcurrencyconverterapi;
 
 import retrofit2.Call;
@@ -9,5 +10,14 @@ public interface ExchangeRateService {
     /**
      * Método para obter taxas de câmbio recentes
      */
+    @GET("/vó/{apiKey}/latest/{baseCurrency}")
+
+    Call<ExchangeRatesResponse> getExchangeRates(
+
+            @Path("apiKey") String apiKey,
+            // Subistitui {apiKey} na URL com valor fornecido
+            @Path("baseCurrency") String baseCurrency,
+            // Subistitui {baseCurrency} na URl com valor fornecido
+    );
 }
 
